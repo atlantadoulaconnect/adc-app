2c581e09685885b271f13771212c27824d1db874
amitej <v.amitej@gmail.com>
2020-01-24 20:24:19 -0500

    created doula application front end

--- adc_app/lib/routes.dart
+++ adc_app/lib/routes.dart
@@ -11,12 +11,23 @@ import 'package:adc_app/screens/applications/client_app.dart';
 import 'package:adc_app/screens/client_signup_screen.dart';
 import 'package:adc_app/screens/doula_signup_screen.dart';
 import 'package:adc_app/screens/client_home_screen.dart';
+import 'package:adc_app/screens/applications/doula_app_page2.dart';
+import 'package:adc_app/screens/applications/doula_app_page3.dart';
+import 'package:adc_app/screens/applications/doula_app_page4.dart';
+import 'package:adc_app/screens/applications/doula_app_page5.dart';
+import 'package:adc_app/screens/applications/doula_app_completion_page.dart';
+
+
 
 final Map<String, WidgetBuilder> routesTable = <String, WidgetBuilder>{
   "/": (BuildContext context) => HomePage(),
   "/login": (BuildContext context) => LoginPage(),
-  "/doulaApp": (BuildContext context) =>
-      DoulaAppPage(title: 'Doula Application'),
+  "/doulaApp": (BuildContext context) => DoulaAppPage(),
+  "/doulaAppPage2": (BuildContext context) => DoulaAppPage2(),
+  "/doulaAppPage3": (BuildContext context) => DoulaAppPage3(),
+  "/doulaAppPage4": (BuildContext context) => DoulaAppPage4(),
+  "/doulaAppPage5": (BuildContext context) => DoulaAppPage5(),
+  "/doulaAppCompletionPage": (BuildContext context) => DoulaAppCompletionPage(),
   "/clientApp": (BuildContext context) => ClientAppPage(),
   "/clientSignup": (BuildContext context) => ClientSignupPage(),
   "/doulaSignup": (BuildContext context) => DoulaSignupPage(),

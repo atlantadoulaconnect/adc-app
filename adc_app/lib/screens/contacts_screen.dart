4834a7ef86636f197877b5346765f265a75c8239
amitej <v.amitej@gmail.com>
2020-02-16 20:50:11 -0500

    minor changes to recent messages screen to improve aesthetics

--- adc_app/lib/screens/contacts_screen.dart
+++ adc_app/lib/screens/contacts_screen.dart
@@ -17,76 +17,80 @@ class _ContactsPageState extends State<ContactsPage> {
     final recentContacts = ["Debbie D.", "Jane D.", "Sarah S."];
     final contactCards = <Widget>[];
     for (var i = 0; i < recentContacts.length; i++) {
-      contactCards.add(Padding(
-        padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 15.0),
-        child: Container(
-          height: 100,
-          decoration: BoxDecoration(
-            border: Border.all(
-                width: 2.0,
+      contactCards.add(
+        Padding(
+          padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 15.0),
+          child: Container(
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(
+                  width: 2.0,
+                ),
+              borderRadius: BorderRadius.all(
+                const Radius.circular(20.0)
               ),
-            borderRadius: BorderRadius.all(
-              const Radius.circular(20.0)
             ),
-          ),
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Container(
-                    width: 65,
-                    height: 65,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        width: 3.0,
-                        color: themeColors["black"],
+            child: MaterialButton(
+              onPressed: () => Navigator.pushNamed(context, '/'),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 3.0,
+                            color: themeColors["black"],
+                          ),
+                        ),
+                        child: Icon(
+                          IconData(0xe7fd, fontFamily: 'MaterialIcons'),
+                          color: Colors.black,
+                          size: 50,
+                        ),
                       ),
                     ),
-                    child: Icon(
-                      IconData(0xe7fd, fontFamily: 'MaterialIcons'),
-                      color: Colors.black,
-                      size: 50,
+                    Padding(
+                      padding: EdgeInsets.only(left: 12, right: 20.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              recentContacts[i],
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Doula",
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          ]
+                      ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 12, right: 20.0),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          recentContacts[i],
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "Doula",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Spacer(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Icon(
-                    IconData(57545, fontFamily: 'MaterialIcons'),
-                    color: Colors.black,
-                    size: 40,
-                  ),
-                ),
-              ]),
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Icon(
+                        IconData(57545, fontFamily: 'MaterialIcons'),
+                        color: Colors.black,
+                        size: 40,
+                      ),
+                    ),
+                  ]),
+            ) 
+          ),
         ),
-      ),
       );
     }
     return Scaffold(
--- adc_app/lib/screens/recent_messages.dart
+++ adc_app/lib/screens/recent_messages.dart
@@ -17,97 +17,101 @@ class _RecentMessagesPageState extends State<RecentMessagesPage> {
     final recentContacts = ["Debbie D.", "Jane D.", "Sarah S."];
     final contactCards = <Widget>[];
     for (var i = 0; i < recentContacts.length; i++) {
-      contactCards.add(Padding(
-        padding: EdgeInsets.only(bottom: 10.0),
-        child: Container(
-          height: 100,
-          decoration: BoxDecoration(
-              border: Border.all(
-                width: 2.0,
-              )
+      contactCards.add(
+        Padding(
+          padding: EdgeInsets.only(bottom: 10.0),
+          child: Container(
+            height: 100,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2.0,
+                )
+            ),
+            child: MaterialButton(
+              onPressed: () => Navigator.pushNamed(context, '/'),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 3.0,
+                            color: themeColors["black"],
+                          ),
+                        ),
+                        child: Icon(
+                          IconData(0xe7fd, fontFamily: 'MaterialIcons'),
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 12.0),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              recentContacts[i],
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Doula",
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              "Recent message will be ...",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: themeColors["coolGray5"]
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 17.0),
+                      child: Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 2.0,
+                            color: themeColors["black"],
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "2",
+                            style: TextStyle(
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                                color: themeColors["black"]
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            )
           ),
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Container(
-                    width: 65,
-                    height: 65,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        width: 3.0,
-                        color: themeColors["black"],
-                      ),
-                    ),
-                    child: Icon(
-                      IconData(0xe7fd, fontFamily: 'MaterialIcons'),
-                      color: Colors.black,
-                      size: 50,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 12.0),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          recentContacts[i],
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "Doula",
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "Recent message will be ...",
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: themeColors["coolGray5"]
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Spacer(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        width: 2.0,
-                        color: themeColors["black"],
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "2",
-                        style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                            color: themeColors["black"]
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ]),
         ),
-      ),
       );
     }
     return Scaffold(
@@ -115,20 +119,20 @@ class _RecentMessagesPageState extends State<RecentMessagesPage> {
           title: Text('Recent Messages'),
           actions: <Widget>[
             Container(
-              width: 60,
+              width: 55,
               child: MaterialButton(
                 onPressed: () => Navigator.pushNamed(context, '/contacts'),
                 child: Icon(
                   IconData(0xe150, fontFamily: 'MaterialIcons'),
                   color: Colors.white,
-                  size: 32,
+                  size: 30,
                 ),
               ),
             ),
           ],
         ),
         body: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: contactCards,

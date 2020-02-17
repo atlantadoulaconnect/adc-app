849d733123cad4e6d99159a02448995148c40598
rstrozensky <ryanstrozensky@yahoo.com>
2020-02-17 18:44:32 -0500

    Add utility time conversion methods

--- adc_app/lib/models/message.dart
+++ adc_app/lib/models/message.dart
@@ -1,20 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 
 class Message {
   String messageId;
   String senderId = "sender";
   String type;
   String content;
-//  Timestamp timeSent;
-  TimeOfDay timeSent;
+  Timestamp timeSent;
   Timestamp timeRead;
 
   Message.now(String content, String senderId) {
-    Message("Content", "Test", new TimeOfDay.now());
+    Message(content, senderId, new Timestamp.now());
   }
 
-  Message(String content, String senderId, TimeOfDay timeSent) {
+  Message(String content, String senderId, Timestamp timeSent) {
     this.content = content;
     this.senderId = senderId;
     this.timeSent = timeSent;

f29130d7986654ec61582127669f4589753bc945
amitej <v.amitej@gmail.com>
2020-02-17 22:41:38 -0500

    text bank added more functionality

--- adc_app/lib/screens/text_bank.dart
+++ adc_app/lib/screens/text_bank.dart
@@ -15,8 +15,68 @@ class _TextBankPageState extends State<TextBankPage> {
     super.initState();
   }
 
+
   @override
   Widget build(BuildContext context) {
+
+    final listOfCategories = ["Category A", "Category B", "Category C",
+                              "Category D", "Category E", "Category F"];
+
+    final listOfQuestions = ["Question 1:", "Question 2:", "Question 3:",
+                             "Question 4:", "Question 5:", "Question 6:"];
+
+    final listOfAnswers = ["Answers 1:", "Answers 2:", "Answers 3:",
+                           "Answers 4:", "Answers 5:", "Answers 6:"];
+
+    final QandATextFields = List<Widget>();
+
+    for (var i = 0; i < listOfQuestions.length; i++) {
+      QandATextFields.add(
+        Text(
+          listOfQuestions[i],
+          style: TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      );
+      QandATextFields.add(
+        Padding(
+          padding: EdgeInsets.only(bottom: 10.0),
+          child: Text(
+            listOfAnswers[i],
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+      );
+    }
+
+    final categoryExpansionTiles = List<Widget>();
+
+    for (var i = 0; i < listOfCategories.length; i++) {
+      categoryExpansionTiles.add(ExpansionTile(
+          title: Text(
+            listOfCategories[i],
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Align(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: QandATextFields,
+                ),
+                alignment: Alignment.centerLeft,
+              ),
+            ),
+          ],
+        ));
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Text Bank")),
@@ -27,41 +87,11 @@ class _TextBankPageState extends State<TextBankPage> {
             ),
           ],
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: Container(
-//                color: Colors.red,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.black,
-                        width: 2.0
-                    ),
-                  ),
-                  child: ExpansionTile(
-                    title: Text('"Category A"'),
-                    children: <Widget>[
-                      Container(
-                        child: Column(
-                          children: <Widget>[
-                            Text("Question 1"),
-                            Text("Answer A:"),
-                            Text("Answer B:"),
-                            Text("Question 2"),
-                            Text("Answer A:"),
-                            Text("Answer B:"),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-              ),
-            ),
-          ],
+        body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+            child: ListView(
+              children: categoryExpansionTiles,
+            )
         )
     );
   }

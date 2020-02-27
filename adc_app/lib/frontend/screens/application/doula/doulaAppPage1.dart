ded6a0f609e385a42c8a4302a9a4f65962333623
shounima <shounimanaeem@gmail.com>
2020-02-27 17:32:16 -0500

    Switching Doula app from Columns to Listview

--- adc_app/lib/frontend/screens/application/doula/doulaAppPage1.dart
+++ adc_app/lib/frontend/screens/application/doula/doulaAppPage1.dart
@@ -60,144 +60,160 @@ class DoulaAppPage1State extends State<DoulaAppPage1> {
           title: Text("Doula Application"),
         ),
         body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
+//            mainAxisAlignment: MainAxisAlignment.center,
+//            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Text(
-                'Personal Information',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  color: themeColors['emoryBlue'],
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Personal Information',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    color: themeColors['emoryBlue'],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
                 ),
               ),
-              Container(
-                width: 250,
-                child: LinearProgressIndicator(
-                  backgroundColor: themeColors['skyBlue'],
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(themeColors['mediumBlue']),
-                  value: 0.2,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 250,
+                  child: LinearProgressIndicator(
+                    backgroundColor: themeColors['skyBlue'],
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(themeColors['mediumBlue']),
+                    value: 0.2,
+                  ),
                 ),
               ),
-              Form(
-                key: _d1formKey,
-                autovalidate: false,
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Flexible(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: "First Name",
-                              hintText: "Jane",
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: _d1formKey,
+                  autovalidate: false,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Flexible(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "First Name",
+                                hintText: "Jane",
+                              ),
+                              controller: _firstNameCtrl,
+                              validator: nameValidator,
                             ),
-                            controller: _firstNameCtrl,
-                            validator: nameValidator,
                           ),
-                        ),
-                        Flexible(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: "Last Initial",
-                              hintText: "D",
+                          Flexible(
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "Last Initial",
+                                hintText: "D",
+                              ),
+                              controller: _lastInitCtrl,
+                              validator: singleLetterValidator,
                             ),
-                            controller: _lastInitCtrl,
-                            validator: singleLetterValidator,
-                          ),
-                        )
-                      ],
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: "Birthday",
-                        hintText: "(MM/YYYY)",
+                          )
+                        ],
                       ),
-                      keyboardType: TextInputType.datetime,
-                      controller: _bdayCtrl,
-                      validator: bdayValidator,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                          labelText: "Phone", hintText: "4045553333"),
-                      keyboardType: TextInputType.number,
-                      controller: _phoneCtrl,
-                      validator: phoneValidator,
-                    ),
-                    TextFormField(
+                      TextFormField(
                         decoration: InputDecoration(
-                            labelText: "Alternate Phone",
-                            hintText: "6784447777"),
-                        keyboardType: TextInputType.number,
-                        controller: _altPhoneCtrl,
-                        validator: (val) {
-                          if (val.isNotEmpty) {
-                            return phoneValidator(val);
-                          }
-                          return null;
-                        }),
-                    Row(children: <Widget>[
-                      RaisedButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(10.0),
-                            side: BorderSide(color: themeColors['mediumBlue'])),
-                        onPressed: () {
-                          // inputted information is lost when previous is pressed
-                          // this should take them home
-                          // TODO nav pop until doula-specific home
-                          Navigator.pushNamed(context, '/home');
-                        },
-                        color: themeColors['mediumBlue'],
-                        textColor: Colors.white,
-                        padding: EdgeInsets.all(15.0),
-                        splashColor: themeColors['mediumBlue'],
-                        child: Text(
-                          "PREVIOUS",
-                          style: TextStyle(fontSize: 20.0),
+                          labelText: "Birthday",
+                          hintText: "(MM/YYYY)",
                         ),
+                        keyboardType: TextInputType.datetime,
+                        controller: _bdayCtrl,
+                        validator: bdayValidator,
                       ),
-                      RaisedButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(10.0),
-                            side: BorderSide(color: themeColors['yellow'])),
-                        onPressed: () {
-                          final form = _d1formKey.currentState;
-                          if (form.validate()) {
-                            form.save();
-
-                            String displayName =
-                                "${_firstNameCtrl.text.toString().trim()} ${_lastInitCtrl.text.toString().trim()}.";
-                            String birthday = _bdayCtrl.text.toString().trim();
-                            List<Phone> phones = List();
-                            phones.add(
-                                Phone(_phoneCtrl.text.toString().trim(), true));
-                            if (_altPhoneCtrl.text.isNotEmpty) {
-                              phones.add(Phone(
-                                  _altPhoneCtrl.text.toString().trim(), false));
+                      TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Phone", hintText: "4045553333"),
+                        keyboardType: TextInputType.number,
+                        controller: _phoneCtrl,
+                        validator: phoneValidator,
+                      ),
+                      TextFormField(
+                          decoration: InputDecoration(
+                              labelText: "Alternate Phone",
+                              hintText: "6784447777"),
+                          keyboardType: TextInputType.number,
+                          controller: _altPhoneCtrl,
+                          validator: (val) {
+                            if (val.isNotEmpty) {
+                              return phoneValidator(val);
                             }
-
-                            updateDoula(
-                                currentUser, displayName, birthday, phones);
-
-                            toDoulaAppPage2();
-                          }
-                        },
-                        color: themeColors['yellow'],
-                        textColor: Colors.white,
-                        padding: EdgeInsets.all(15.0),
-                        splashColor: themeColors['yellow'],
-                        child: Text(
-                          "NEXT",
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            color: themeColors['black'],
+                            return null;
+                          }),
+                      Row(children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(10.0),
+                                side: BorderSide(color: themeColors['mediumBlue'])),
+                            onPressed: () {
+                              // inputted information is lost when previous is pressed
+                              // this should take them home
+                              // TODO nav pop until doula-specific home
+                              Navigator.pushNamed(context, '/home');
+                            },
+                            color: themeColors['mediumBlue'],
+                            textColor: Colors.white,
+                            padding: EdgeInsets.all(15.0),
+                            splashColor: themeColors['mediumBlue'],
+                            child: Text(
+                              "PREVIOUS",
+                              style: TextStyle(fontSize: 20.0),
+                            ),
                           ),
                         ),
-                      ),
-                    ]),
-                  ],
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(10.0),
+                                side: BorderSide(color: themeColors['yellow'])),
+                            onPressed: () {
+                              final form = _d1formKey.currentState;
+                              if (form.validate()) {
+                                form.save();
+
+                                String displayName =
+                                    "${_firstNameCtrl.text.toString().trim()} ${_lastInitCtrl.text.toString().trim()}.";
+                                String birthday = _bdayCtrl.text.toString().trim();
+                                List<Phone> phones = List();
+                                phones.add(
+                                    Phone(_phoneCtrl.text.toString().trim(), true));
+                                if (_altPhoneCtrl.text.isNotEmpty) {
+                                  phones.add(Phone(
+                                      _altPhoneCtrl.text.toString().trim(), false));
+                                }
+
+                                updateDoula(
+                                    currentUser, displayName, birthday, phones);
+
+                                toDoulaAppPage2();
+                              }
+                            },
+                            color: themeColors['yellow'],
+                            textColor: Colors.white,
+                            padding: EdgeInsets.all(15.0),
+                            splashColor: themeColors['yellow'],
+                            child: Text(
+                              "NEXT",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: themeColors['black'],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]),
+                    ],
+                  ),
                 ),
               )
             ],
@@ -246,6 +262,7 @@ class ViewModel extends BaseModel<AppState> {
               name: name,
               phones: phones,
               bday: bday,
-            )));
+            ))
+    );
   }
 }

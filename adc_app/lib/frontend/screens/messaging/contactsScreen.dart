d0ad8fe0273e232451cfeba691e7f9a774f2d199
shounima <shounimanaeem@gmail.com>
2020-03-02 18:02:23 -0500

    Relative sizing for contacts screen

--- adc_app/lib/frontend/screens/messaging/contactsScreen.dart
+++ adc_app/lib/frontend/screens/messaging/contactsScreen.dart
@@ -21,69 +21,70 @@ class ContactsScreen extends StatelessWidget {
     if (doc["userid"] == currentUser.userid) {
       return Container();
     } else {
-      return Container(
-        height: 100,
-        decoration: BoxDecoration(
-            border: Border.all(width: 2.0),
-            borderRadius: BorderRadius.all(const Radius.circular(20.0))),
-        child: MaterialButton(
-            onPressed: () {
-              // set peer to selected user
-              Contact peer = Contact(
-                  doc["name"],
-                  doc["userid"],
-                  doc["userType"],
-                  calcThreadId(currentUser.userid, doc["userid"]));
-              setPeer(peer);
-              toMessages();
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15.0),
-                    child: Container(
-                      width: 65,
-                      height: 65,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                              width: 3.0, color: themeColors["black"])),
-                      child: Icon(
-                        // TODO replace with user profile picture
-                        IconData(0xe7fd, fontFamily: 'MaterialIcons'),
-                        color: Colors.black,
-                        size: 50,
-                      ),
-                    )),
-                Padding(
-                  padding: EdgeInsets.only(left: 12, right: 20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                          // user name
-                          doc["name"],
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold)),
-                      Text(doc["userType"], style: TextStyle(fontSize: 20))
-                    ],
+      return Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+              border: Border.all(width: 2.0),
+              borderRadius: BorderRadius.all(const Radius.circular(20.0))),
+          child: MaterialButton(
+              onPressed: () {
+                // set peer to selected user
+                Contact peer = Contact(
+                    doc["name"],
+                    doc["userid"],
+                    doc["userType"],
+                    calcThreadId(currentUser.userid, doc["userid"]));
+                setPeer(peer);
+                toMessages();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Container(
+                        width: 65,
+                        height: 65,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                                width: 3.0, color: themeColors["black"])),
+                        child: Icon(
+                          // TODO replace with user profile picture
+                          IconData(0xe7fd, fontFamily: 'MaterialIcons'),
+                          color: Colors.black,
+                          size: 50,
+                        ),
+                      )),
+                  Padding(
+                    //padding: EdgeInsets.only(left: 12, right: 20.0),
+                    padding: EdgeInsets.all(5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                            // user name
+                            doc["name"],
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold)),
+                        Text(doc["userType"], style: TextStyle(fontSize: 20))
+                      ],
+                    ),
                   ),
-                ),
-                Spacer(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Icon(
+                  //Spacer(),
+                  Icon(
                     IconData(57545, fontFamily: 'MaterialIcons'),
                     color: Colors.black,
                     size: 40,
                   ),
-                ),
-              ],
-            )),
+                ],
+              )),
+        ),
       );
     }
   }
